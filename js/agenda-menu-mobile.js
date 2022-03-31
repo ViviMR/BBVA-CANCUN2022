40f2a9edb33da56989be3c2c747bb372bde453de
ViviMR <psrangel.6@gmail.com>
2022-03-31 15:57:02 -0500

    se agregan nuevas clases en html, css y  funcionales de js en agenda-menu-mobile

--- js/agenda-menu-mobile.js
+++ js/agenda-menu-mobile.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo")
+let botonMenuMobile = document.querySelector('.logo-container-button-menu-mobile')
+let menuMobileContent = document.querySelector('.menu-mobile')
+let botonMenuClose = document.querySelector('.menu-mobile-close')
+
+botonMenuMobile.addEventListener('click', () => {
+    menuMobileContent.classList.toggle('menu-mobile--show')
+})
+
+botonMenuClose.addEventListener('click', () => {
+    menuMobileContent.classList.toggle('menu-mobile--show')
+})
+
